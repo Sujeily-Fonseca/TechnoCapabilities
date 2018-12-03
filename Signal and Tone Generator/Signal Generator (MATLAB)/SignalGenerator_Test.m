937afb750851ee8fe1b7d59db04a50b7e9aa8c74
SFonseca <sujeily.fonseca@upr.edu>
2018-12-03 15:51:51 -0400

    Added ability to generate sawtooth signals
    The Signal Generator function was updated so that it could generate sawtooth signals, which was optimal in practice. Furthermore, the test script was updated to verify the generation of the new signal, invalid parameters, or incorrect number of arguments

--- Signal and Tone Generator/Signal Generator (MATLAB)/SignalGenerator_Test.m
+++ Signal and Tone Generator/Signal Generator (MATLAB)/SignalGenerator_Test.m
@@ -1,9 +1,9 @@
 % Project: Hands-Free Intraoral Electrolarynx
 % Module: Signal and Tone Generator 
-% Description: This script is intended to test the SignalGenerator
-% function with three frequency values (120Hz for men, 200Hz for 
-% women, and 300Hz for children). These frecuency values correspond to the
-% pitch options available in the Hands-Free Intraoral Electrolarynx.
+% Description: This script is intended to test the SignalGenerator function 
+% with three frequency values (120Hz for low frequency, 200Hz for medium 
+% frequency, and 300Hz for high frequency). These frecuency values correspond 
+% to the pitch options available in the Hands-Free Intraoral Electrolarynx.
 % Author: Sujeily P. Fonseca-Gonzalez
 % Username: Sujeily-Fonseca
 
@@ -11,15 +11,32 @@ clear all % Clear variables and functions
 close all % Close all the open figure windows
 clc       % Clear command window
 
-% Test No 1: 120 Hz
-ImpulseTrain_120Hz = SignalGenerator(120); % Impulse train using 120Hz (men)
-pause(10);                                 % Wait 10 seconds 
+% Test No 1: 120Hz Impulse Train Signal
+SignalGenerator(120, "impulse");  % Impulse train signal using 120Hz 
+pause(10);                        % Wait 10 seconds 
 
-% Test No 2: 200 Hz
-ImpulseTrain_200Hz = SignalGenerator(200); % Impulse train using 200Hz (women)
-pause(10);                                 % Wait 10 seconds 
+% Test No 2: 200Hz Impulse Train Signal
+SignalGenerator(200, "impulse");  % Impulse train signal using 200Hz 
+pause(10);                        % Wait 10 seconds 
 
-% Test No 3: 300 Hz
-ImpulseTrain_300Hz = SignalGenerator(300); % Impulse train using 300Hz (children)
-pause(10);                                 % Wait 10 seconds 
+% Test No 3: 300Hz Impulse Train Signal
+SignalGenerator(120, "impulse");  % Impulse train signal using 300Hz 
+pause(10);                        % Wait 10 seconds 
 
+% Test No 4: 120Hz Sawtooth Signal
+SignalGenerator(120, "sawtooth"); % Sawtooth signal using 120Hz
+pause(10);                        % Wait 10 seconds 
+
+% Test No 5: 200Hz Sawtooth Signal
+SignalGenerator(200, "sawtooth"); % Sawtooth signal using 200Hz 
+pause(10);                        % Wait 10 seconds 
+
+% Test No 6: 300Hz Sawtooth Signal
+SignalGenerator(300, "sawtooth"); % Sawtooth signal using 300Hz 
+pause(10);                        % Wait 10 seconds 
+
+% Test No 7: Invalid Parameter
+SignalGenerator(120, "test");
+
+% Test No 8: Not Enough Imput Arguments
+SignalGenerator(120);
