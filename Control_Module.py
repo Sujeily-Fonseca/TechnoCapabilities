dd685b208287839b441b279d8e41242a5592b9d2
gilissaM <gilissa.matos@upr.edu>
2018-12-04 01:23:47 -0400

    Update Control_Module.py
    A new volume function is used from the module Sound.  An ON/OFF green led was added to indicate the status of the system.   Also, a detail documentation was added.

--- Control_Module.py
+++ Control_Module.py
@@ -13,8 +13,9 @@ Description: Program that controls any interaction with the user.
 
 """ Importing external modules """
 import RPi.GPIO as GPIO
-import subprocess
-
+from ToneGenerator import toneGenerator
+from ToneGenerator import increaseVolume
+from ToneGenerator import decreaseVolume
 
 """ Global Variables Declaration and Assignment """
 signal_status = 0                                        # 0- Signal is deactivated     1- Signal is activated     
@@ -30,25 +31,25 @@ pitch_tracker = 0                                        # Pointer to the pitch 
 
 # The up_flag is set only if the signal is active
 def upHandler(pin):
-    global up_flag, signal_status
+    global up_flag, signal_status                        # Get access to the global variable
     if signal_status:
-        up_flag = 1
+        up_flag = 1                                      # Set volume up flag to 1
         
 # The up_flag is set only if the signal is active
 def downHandler(pin):
-    global down_flag, signal_status
+    global down_flag, signal_status                      # Get access to the global variable
     if signal_status:
-        down_flag = 1
+        down_flag = 1                                    # Set volume down flag to 1
 
 def signalHandler(pin):
-    global signal_flag
-    signal_flag = 1
+    global signal_flag                                   # Get access to the global variable
+    signal_flag = 1                                      # Set signal flag to 1
 
 # The pitch_flag is set only if the signal is deactived
 def pitchHandler(pin):
-    global pitch_flag, signal_status
-    if signal_status == 0: 
-        pitch_flag = 1
+    global pitch_flag, signal_status                     # Get access to the global variable
+    if signal_status == 0:    
+        pitch_flag = 1                                   # Set the pitch flag to 1
 
 """ Subroutines for volume, pitch, and signal control """
 
@@ -56,30 +57,19 @@ def pitchHandler(pin):
 def changePitch():
     global pitch_tracker
     if pitch_tracker == 0:
-        GPIO.output(25, GPIO.LOW)
-        GPIO.output(8,GPIO.HIGH)
-        pitch_tracker +=1
+        GPIO.output(25, GPIO.LOW)                       # Turn off the blue led 
+        GPIO.output(8,GPIO.HIGH)                        # Turn on the red led 
+        pitch_tracker +=1                               # Change the pitch pointer
 
     elif pitch_tracker == 1:
-        GPIO.output(8, GPIO.LOW)
-        GPIO.output(7, GPIO.HIGH)
-        pitch_tracker += 1
+        GPIO.output(8, GPIO.LOW)                        # Turn off the red led
+        GPIO.output(7, GPIO.HIGH)                       # Turn on the yellow led
+        pitch_tracker += 1                              # Change the pitch pointer
         
     else:
-        GPIO.output(7, GPIO.LOW)
-        GPIO.output(25, GPIO.HIGH)
-        pitch_tracker = 0
-            
-    print("The selected pitch is: %d." % (pitch[pitch_tracker]))
-
-# Function decrementVolume() decrements the PCM volume on card 0 using amixer commands 
-def decrementVolume():
-    subprocess.Popen(['amixer','set','PCM','10db-'], shell=False)
-
-# Function decrementVolume() increments the PCM volume on card 0 using amixer commands
-def incrementVolume():
-    subprocess.Popen(['amixer','set','PCM','10db+'], shell= False)
-
+        GPIO.output(7, GPIO.LOW)                        # Turn off the yellow pointer
+        GPIO.output(25, GPIO.HIGH)                      # Turn on the blue led
+        pitch_tracker = 0                               # Change the pitch pointer
 
 """ Ports configuration and callback assignments """
 GPIO.setmode(GPIO.BCM)                                   # Referring to the pins by the Broadcom SoC channel number
@@ -89,15 +79,16 @@ GPIO.setup(4, GPIO.IN)                                   # Increase Volume Butto
 GPIO.setup(10, GPIO.IN)                                  # Decrease Volume Button assigned to GPIO 10
 GPIO.setup(15, GPIO.IN)                                  # Signal Activation/Deactivation Button and PSS assigned to GPIO 17
 GPIO.setup(23, GPIO.IN)                                  # Pitch Button assigned to GPIO 22
-GPIO.setup(25, GPIO.OUT, initial=GPIO.HIGH)                                  # 120Hz indicator - Male pitch
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)                                  # 200Hz indicator - Female pitch
-GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)                                  # 300Hz indicator - Child pitch
+GPIO.setup(12, GPIO.OUT, initial=GPIO.HIGH)              # On/Off led indicator
+GPIO.setup(25, GPIO.OUT, initial=GPIO.HIGH)              # 120Hz led indicator - Low pitch
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)                # 200Hz led indicator - Medium pitch
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)                # 300Hz led indicator - High pitch
 
-# Callback assignments for GPIO 10, 4, 17, and 22.
-GPIO.add_event_detect(10, GPIO.FALLING, callback = downHandler, bouncetime = 2)
-GPIO.add_event_detect(4, GPIO.FALLING, callback = upHandler, bouncetime = 2)
-GPIO.add_event_detect(15, GPIO.FALLING, callback = signalHandler, bouncetime = 2)
-GPIO.add_event_detect(23, GPIO.FALLING, callback = pitchHandler, bouncetime = 2)
+# Callback assignments for GPIO 10, 4, 15, and 22.3
+GPIO.add_event_detect(10, GPIO.FALLING, callback = downHandler, bouncetime = 250)   # Volume down button
+GPIO.add_event_detect(4, GPIO.FALLING, callback = upHandler, bouncetime = 250)      # Volume up button
+GPIO.add_event_detect(15, GPIO.FALLING, callback = signalHandler, bouncetime = 250) # Signal activation/deactivation button
+GPIO.add_event_detect(23, GPIO.FALLING, callback = pitchHandler, bouncetime = 250)  # Pitch button
 
 """ Wait for an interrupt """
 
@@ -106,28 +97,25 @@ try:
 
         if signal_flag:
             signal_flag = 0                                         # Reset signal_flag
-            if signal_status:
-                #Signal Deactivation Code
+            if signal_status:                                       # Signal deactivation
                 signal_status = 0                                   # Reset signal_status
-                print("Disable Signal!")
-            else:
-                #Signal Activation Code
+            else:                                                   # Signal activation
                 signal_status = 1                                   # Set signal_status
-                print("Enable Signal!")
+            toneGenerator(pitch[pitch_tracker], signal_status)      # Call toneGenerator function
         elif up_flag:
             up_flag = 0                                             # Reset up_flag
-            incrementVolume()                                       # Increment the RPi volume
+            increaseVolume()                                       # Increment the RPi volume
 
         elif down_flag:
             down_flag = 0                                           # Reset down_flag
-            decrementVolume()                                       # Decrement the RPi volume
+            decreaseVolume()                                       # Decrement the RPi volume
 
         elif pitch_flag:
             pitch_flag = 0                                          # Reset pitch_flag
             changePitch()                                           # Change pitch 
 
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    GPIO.cleanup()                                                  # Clean all used ports 
 
 finally:
     GPIO.cleanup()                                                  # Clean exit
